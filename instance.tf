8c4c87cad53829cc189e7ed846443dd613976c0a
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-07-31 14:54:18 +0200

    correct syntax

--- instance.tf
+++ instance.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "Baklava" {
   }
 
   # Instanz mit Security-Group verknüpfen
-  vpc_security_group_ids = [aws_security_group.allow_ingress.id]
-  subnet_id =aws_subnet.kugelfisch1.id
+  vpc_security_group_ids      = [aws_security_group.allow_ingress.id]
+  subnet_id                   = aws_subnet.kugelfisch1.id
   associate_public_ip_address = true
 
 }
